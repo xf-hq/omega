8a89f1c717ed060fb381aca9f7003b3fdb104a73
Nathan Ridley <axefrog@gmail.com>
2025-05-22 19:15:51 +1000

    Deleted obsolete schema.ts file

--- source/red-engine/schema.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod/v4';
-import { ECookingPlatform, EMaterialPriority, EMaterialShadingRateMode, ERenderingMaterialType, ETextureAddressing, ETextureComparisonFunction, ETextureFilteringMag, ETextureFilteringMin, ETextureFilteringMip, PSODescBlendModeFactor, PSODescBlendModeOp, PSODescBlendModeWriteMask, PSODescDepthStencilModeComparisonMode, PSODescDepthStencilModeStencilOpMode, PSODescRasterizerModeCullMode, PSODescRasterizerModeFrontFaceWinding, PSODescRasterizerModeOffsetMode, ResourceFlags } from './enums-and-flags';
-
-export namespace Schema {
-}
